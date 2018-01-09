d1f1ea0880766434d0dfc59261c74bd4eb7d4cb6
Phillis <phillis.y@gmail.com>
2018-01-08 21:59:51 -0500

    all test cases passed but the function is very ugly!

--- planMeal.js
+++ planMeal.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 /**
   * 
   * @param {object} foodRatios
@@ -14,39 +12,97 @@ function planMeal(foodRatios, numPortions) {
 		remainder,
 		portionValue;
 
-	foodRatios = foodRatios || {};
+	// Validate the input
 
-	// get the total of the ratios so we can do math!
+	if (!foodRatios || typeof foodRatios !== "object") {
+		throw "invalid input: foodRatios";
+	}
+
+	if (numPortions === undefined || typeof numPortions !== "number") {
+		throw "invalid input: numPortions";
+	}
+
+	// Get the ratio total
 	Object.keys(foodRatios).forEach(function (k) {
 		total += foodRatios[k];
 	});
+
+
+
 
 	// If the total is 0, simply return the original foodRatios
 	if (!total) {
 		return foodRatios;
 	}
 
-	// Calculate the value of 1 portion
-	portionValue = Math.floor(numPortions / total);
+	var foodRatiosArr = [];
+	var usedTotal = 0;
 
-	// For uneven divides, let's get the remainder so we can spread it to the portions
-	remainder = numPortions % total;
-
-	// Assign a value to each food item based on the original ratio * portionValue
 	Object.keys(foodRatios).forEach(function (k) {
+		var foodRatio = {
+			"id": k,
+			"remainder": (foodRatios[k] * numPortions) % total
+		};
 
-		// If a remainder still exists and this current item is non-zero
-		if (remainder && foodRatios[k]) {
-			ret[k] = (foodRatios[k] * portionValue) + 1;
-			remainder--;
-		} else {
-			ret[k] = foodRatios[k] * portionValue;
-		}
+		// Push it into an array. We can sort by remainder followed by id after.
+		foodRatiosArr.push(foodRatio);
+
+		// Assign its final portion count without remainders taken into account
+		var c = Math.floor(foodRatios[k] * numPortions / total);
+		usedTotal += c;
+		ret[k] = c;
 	});
+
+	remainder =  numPortions - usedTotal;
+
+	// Distribute the remainder based on the sorted foodRatiosArr
+
+	var remainderIdSort = function (a, b) {
+		if (a.remainder > b.remainder) {
+			return -1;
+		}
+
+		if (a.remainder < b.remainder) {
+			return 1;
+		}
+
+		// Otherwise the remainders are even
+		if (a.id > b.id) {
+			return 1;
+		}
+
+		if (a.id < b.id) {
+			return -1;
+		}
+
+		return 0;
+	}
+
+
+	foodRatiosArr.sort(remainderIdSort);
+
+
+	var i = 0;
+	while (remainder) {
+
+		var key = foodRatiosArr[i].id;
+		ret[key]++;
+		i++;
+		remainder--;
+	}
 
 	return ret;
 }
 
-module.exports = planMeal;
+function main(foodRatios, numPortions) {
+	try {
+		return planMeal(foodRatios, numPortions);
+	} catch (e) {
+		console.log(e);
+		return { "error": e };
+	}
+}
+
+module.exports = main;
 
 return module.exports;
